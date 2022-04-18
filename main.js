6bcacaf1a115e6860c19bd8afe6392373f7b291d
Saksham <saksham16dec@gmail.com>
2022-04-18 13:54:49 +0530

    version 0

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById("blog1").onclick = function () {
+    location.href = "blog1.html";
+  };
+document.getElementById("blog2").onclick = function () {
+    location.href = "blog2.html";
+  };
+document.getElementById("blog3").onclick = function () {
+    location.href = "blog3.html";
+  };
